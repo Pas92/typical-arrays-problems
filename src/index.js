41a567bae6d37920cbc5fd8b20610e5c8dc49624
Pas92 <pavelburdovitsin92@gmail.com>
2021-08-13 12:29:12 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0
+    }
+
+    let min = array[0];
+    array.forEach(elem => {
+        if (elem <= min) {
+            min = elem;
+        }
+    });
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0
+    }
+
+    let max = array[0];
+    array.forEach(elem => {
+        if (elem >= max) {
+            max = elem;
+        }
+    });
+
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0
+    }
+    
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    
+     return sum/array.length;
+    
 }
